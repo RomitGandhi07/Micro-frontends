2813e68a81e54b69cfc041ebf175c813ba550916
Romit Gandhi <gandhiromit77@gmail.com>
2022-02-13 12:58:16 +0530

    Added auth app

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -1,28 +1,23 @@
-const { merge } = require("webpack-merge");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-
-const commonConfig = require("./webpack.common");
-const packageJSON = require("../package.json");
-
-const domain = process.env.PRODUCTION_DOMAIN;
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+const commonConfig = require('./webpack.common');
 
 const prodConfig = {
-  mode: "production",
+  mode: 'production',
   output: {
-    filename: "[name].[contenthash].js",
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/',
   },
-  devServer: {
-    port: 8081
-},
   plugins: [
     new ModuleFederationPlugin({
-        name: 'marketing',
-        filename: 'remoteEntry.js',
-        exposes: {
-            './MarketingApp': './src/bootstrap'
-        },
-        shared: packageJSON.dependencies,
-    })
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
   ],
 };
 
